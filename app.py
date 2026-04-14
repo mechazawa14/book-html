bb412b5d21c1f7ecf51ef764d67717ded53ab2cf
mechazawa14 <armay1405@gmail.com>
2026-04-14 08:24:39 +0530

    practice

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-if __name__ == "__main__":
-    app.run(debug = True , port = 5000)
+# hello
